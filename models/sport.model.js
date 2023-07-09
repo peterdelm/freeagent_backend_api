03e50af1f9b6aef530b78ec71fcc2cac39b016e9
Peter Del Mastro <72809827+CabbageConundrum@users.noreply.github.com>
2023-07-08 21:18:32 -0400

    update

--- models/sport.model.js
+++ models/sport.model.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
     calibre: {
-      type: Sequelize.DATE,
+      type: Sequelize.STRING,
     },
     position: {
       type: Sequelize.STRING,
