3e0ff165c9a46ddecf6e101e9fa0af53ba770d10
peterdelm <peterdelmastro@hotmail.com>
2024-02-10 01:14:41 -0500

    wip

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 // };
 
-// app.use(cors("*"));
+app.use(cors("*"));
 
 // parse requests of content-type - application/json
 app.use(express.json());
